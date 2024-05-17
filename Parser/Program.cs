282775dec10d4d7a2a71c737170f0cb8c2bbb7c1
Daniil Taushkanov <118836201+Dalkory@users.noreply.github.com>
2024-05-17 14:29:28 +0300

    v2.0

--- Parser/Program.cs
+++ Parser/Program.cs
@@ -1,17 +1,24 @@
-﻿using MongoDB.Driver;
+using MongoDB.Driver;
 
 class Program
 {
     static void Main(string[] args)
     {
-        var connectionString = "mongodb://localhost:27017";
-        var client = new MongoClient(connectionString);
-        var database = client.GetDatabase("SensorDatabase");
-        var collection = database.GetCollection<SensorData>("SensorDataCollection");
+        try
+        {
+            var connectionString = "mongodb://localhost:27017";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("SensorDatabase");
+            var collection = database.GetCollection<SensorData>("SensorDataCollection");
 
-        var repository = new SensorDataRepository(collection);
-        var parser = new SensorDataParser(repository);
+            var repository = new SensorDataRepository(collection);
+            var parser = new SensorDataParser(repository);
 
-        parser.ParseAndStoreData("OMNI-mLog.txt");
+            parser.ParseAndStoreData("DH_HS5_HS2_E2_Data_corr_V2.csv");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
     }
 }
